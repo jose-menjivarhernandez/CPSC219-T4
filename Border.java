bceb59ce110a12686773fc96053a1d87c13136d8
Nayc1209 <47436701+Nayc1209@users.noreply.github.com>
2019-03-11 15:14:56 -0600

    border && mazes

--- Border.java
+++ Border.java
@@ -8,22 +8,22 @@ import javafx.stage.*;
 
 public class Border extends Application{
 	public void start(Stage primaryStage)throws Exception{
-		Rectangle rect1 = new Rectangle(0,0,900,10);
+		Rectangle rect1 = new Rectangle(0,0,684,19);
 		rect1.setFill(Color.CHOCOLATE);
 		
-		Rectangle rect2 = new Rectangle(0,0,10,650);
+		Rectangle rect2 = new Rectangle(0,0,19,494);
 		rect2.setFill(Color.CHOCOLATE);
 		
-		Rectangle rect3 = new Rectangle(890,0,10,650);
+		Rectangle rect3 = new Rectangle(665,0,19,494);
 		rect3.setFill(Color.CHOCOLATE);
 		
-		Rectangle rect4 = new Rectangle(0,640,900,10);
+		Rectangle rect4 = new Rectangle(0,475,684,19);
 		rect4.setFill(Color.CHOCOLATE);
 		
 		
 		
 		Pane root = new Pane();
-		Scene scene = new Scene(root, 900, 650);
+		Scene scene = new Scene(root, 684, 494);
 		root.getChildren().addAll(rect1, rect2, rect3, rect4);
 		primaryStage.setTitle("Border");
 		primaryStage.setScene(scene);
